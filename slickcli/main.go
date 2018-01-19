e3dfdc6805987f8a507bca32f1effeae628cd4a9
Jason Corbett <jason.corbett@vivint.com>
2018-01-19 14:37:00 -0700

    parsing of configuration now working

--- slickcli/main.go
+++ slickcli/main.go
@@ -1,11 +1,10 @@
 package main
 
 import "github.com/serussell/logxi/v1"
+import "github.com/jasoncorbett/goslick/slickconfig"
 
 func main() {
 	logger := log.New("slickcli")
-	logger.Debug("Debug Message", "foo", "bar")
-	logger.Info("Info Message", "foo", "bar")
-	logger.Warn("Warn Message", "foo", "bar")
-	logger.Error("Error Message", "foo", "bar")
+	slickconfig.Configuration.LoadFromStandardLocations()
+	logger.Info("Configuration parsed.")
 }
--- slickconfig/slickconfig.go
+++ slickconfig/slickconfig.go
@@ -3,12 +3,17 @@ package slickconfig
 import (
 	"os"
 	"strings"
+	"github.com/pelletier/go-toml"
+	"io/ioutil"
+	"net/http"
+	"github.com/serussell/logxi/v1"
+	"fmt"
 )
 
 type SlickConfiguration struct {
 	BaseUrl string `toml:"base-url" comment:"You must supply a base url for slick"`
-	JWTPrivateKey []byte `toml:"jwt-private-key" comment:"The private key used to sign the jwt tokens.  Generate using openssl genrsa -out app.rsa 1024"`
-	JWTPublicKey []byte `toml:"jwt-public-key" comment:"The public key used to verify the jwt tokens.  Generate using openssl rsa -in app.rsa -pubout > app.rsa.pub"`
+	JWTPrivateKey string `toml:"jwt-private-key" comment:"The private key used to sign the jwt tokens.  Generate using openssl genrsa -out app.rsa 1024"`
+	JWTPublicKey string `toml:"jwt-public-key" comment:"The public key used to verify the jwt tokens.  Generate using openssl rsa -in app.rsa -pubout > app.rsa.pub"`
 }
 
 const (
@@ -19,40 +24,61 @@ const (
 
 var (
 	Configuration SlickConfiguration
+	logger = log.New("slickconfig")
 )
 
 func init() {
 	Configuration.BaseUrl = "http://localhost:6789"
 }
 
-func (c *SlickConfiguration) loadFromFile(location string) {
-	
-}
-
-func (c *SlickConfiguration) loadFromUrl(location string) {
-
-}
-
-func (c *SlickConfiguration) Load() {
+func (c *SlickConfiguration) LoadFromStandardLocations() {
 	// load from the location in the environment variable if it's set, next look at the local directory, finally
 	// system location
 	location, ok := os.LookupEnv(ConfigurationEnvironmentVariableName)
 	if !ok {
-		if _, err := os.Stat(locationLocal); os.IsNotExist(err) {
-			location = locationSystem
+		data, err := ioutil.ReadFile(locationLocal)
+		if err == nil {
+			c.Load(data)
 		} else {
-			location = locationSystem
+			data, err = ioutil.ReadFile(locationSystem)
+			if err == nil {
+				c.Load(data)
+			} else {
+				logger.Warn("Unable to find configuration in any of the standard locations, only defaults available.")
+			}
 		}
+	} else {
+		c.LoadFromLocation(location)
 	}
-	c.LoadFromLocation(location)
+}
+
+func (c *SlickConfiguration) Load(data []byte) {
+	err := toml.Unmarshal(data, c)
+	if err != nil {
+		logger.Warn("Problem loading configuration", "error", err)
+	}
 }
 
 func (c *SlickConfiguration) LoadFromLocation(location string) {
 	if strings.HasPrefix(location, "http") {
-		c.loadFromUrl(location)
+		resp, err := http.Get("http://example.com/")
+		defer resp.Body.Close()
+		if err == nil {
+			body, err := ioutil.ReadAll(resp.Body)
+			if err == nil {
+				c.Load(body)
+			} else {
+				logger.Warn(fmt.Sprintf("Error retrieving logs from location %s, only defaults available.", location), "error", err)
+			}
+		} else {
+			logger.Warn(fmt.Sprintf("Error retrieving logs from location %s, only defaults available.", location), "error", err)
+		}
 	} else {
-		if _, err := os.Stat(location); err == nil {
-			c.loadFromFile(location)
+		data, err := ioutil.ReadFile(location)
+		if err == nil {
+			c.Load(data)
+		} else {
+			logger.Warn(fmt.Sprintf("Error retrieving logs from location %s, only defaults available.", location), "error", err)
 		}
 	}
 }
